27d4ef4b75310f3537dcc0710b62f7a5b943569a
azeddinekhawla@gmail.com <azeddinekhawla@gmail.com>
2024-12-07 20:41:57 +0100

    Add Client442 Project to new branch

---
+++ src/main/java/ClientWS.java
@@ -0,0 +1,13 @@
+import proxyy.BanqueService;
+import proxyy.BanqueWS;
+import proxyy.Compte;
+
+public class ClientWS {
+    public static void main(String[] args) {
+        BanqueService stud=new BanqueWS().getBanqueServicePort();
+        System.out.println(stud.convert(7600));
+        Compte cp=stud.getCompte(5);
+        System.out.println(cp.getCode());
+        System.out.println(cp.getSolde());
+    }
+}
